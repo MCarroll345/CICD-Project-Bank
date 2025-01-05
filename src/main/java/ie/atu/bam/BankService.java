e9d95e96480270a5fa18ccbf5cc37038adb7407d
Mark <G00419267@atu.ie>
2025-01-05 23:24:53 +0000

    Withdrawal/Deposit now working

--- src/main/java/ie/atu/bam/BankService.java
+++ src/main/java/ie/atu/bam/BankService.java
@@ -1,9 +1,12 @@
 package ie.atu.bam;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class BankService {
@@ -21,6 +24,9 @@ public class BankService {
     public String createA(Long uID){
         BankAccount bankAccount = new BankAccount();
         int randIBAN = (int)(Math.random() * 10001);
+        if (bankRepository.existsByuID(uID)){
+            return "Account already exists";
+        }
 
         while(!bankRepository.existsByIBAN(randIBAN)){
             bankAccount.setBalance(0);
@@ -33,8 +39,31 @@ public class BankService {
         return null;
     }
 
-    public List<BankAccount> loginAcc(Long uID){
+    public List<Object> loginAcc(Long uID){
         return bankRepository.findByuID(uID);
+    }
+
+    public ResponseEntity<String> withDep(Long uID, String inout, float num){
+        float newBlnc = 0;
+        switch (inout){
+            case "withdraw":
+                if(num > bankRepository.findBalance(uID)){
+                    return new ResponseEntity<>("Insufficient balance", HttpStatus.BAD_REQUEST);
+                }
+                else{
+                    newBlnc = bankRepository.findBalance(uID) - num;
+                    bankRepository.balanceUpdate(uID, newBlnc);
+                    return new ResponseEntity<>("Withdrawal made, new balance: " + newBlnc, HttpStatus.OK);
+                }
+
+            case "deposit":
+                newBlnc = bankRepository.findBalance(uID) + num;
+                bankRepository.balanceUpdate(uID, newBlnc);
+                return new ResponseEntity<>("Deposit made, new balance: " + newBlnc, HttpStatus.OK);
+
+            default:
+                return new ResponseEntity<>("Error", HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
